b0471c98baad39631d76269630f16c771aad97ba
camposLuis <luis.campos.henrique@hotmail.com>
2019-07-16 18:43:00 -0300

    CRUD

--- AmbienteEConceito/index.js
+++ AmbienteEConceito/index.js
@@ -2,11 +2,40 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 const users = ["Diego", "Cláudio", "Victor Hugo"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
 
 server.get("/users/:index", (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
