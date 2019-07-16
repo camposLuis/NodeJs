0e626c4aa2138adff050cfd6d797bb76bed1d119
camposLuis <luis.campos.henrique@hotmail.com>
2019-07-16 17:04:37 -0300

    Query & Route params

--- AmbienteEConceito/index.js
+++ AmbienteEConceito/index.js
@@ -2,10 +2,9 @@ const express = require("express");
 
 const server = express();
 
-// localhost:3000/teste
-
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+server.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json({ message: `Buscando o usuário de código: ${id}` });
 });
 
 server.listen(3000);
