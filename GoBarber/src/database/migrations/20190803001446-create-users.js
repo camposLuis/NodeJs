d9bba2690fea95208bfa34eb320e96eaf2e96763
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-02 21:55:33 -0300

    Model de usuário

---
+++ GoBarber/src/database/migrations/20190803001446-create-users.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      /* avatar: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      }, */
+      password_hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      provider: {
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
