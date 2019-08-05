b1e7dde798ab5ed8c88c820dd72d462f33d0c52c
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-05 08:29:42 -0300

    Cadastro de usuários

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Luís Campos',
-    email: 'luiscampos@hotmail.com',
-    password_hash: '123456789',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
