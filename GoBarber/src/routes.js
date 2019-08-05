336f513edb5acbb460224b2658e2c31170e9ec2b
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-05 08:12:01 -0300

    Criando loader de models

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Luís Campos',
+    email: 'luiscampos@hotmail.com',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
