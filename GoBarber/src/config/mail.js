a36a232ee58fb57ab011956d81d41275911acfe5
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-15 11:20:52 -0300

    Configurando Nodemailer

---
+++ GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '9131be913f91cf',
+    pass: '5e661789bfb403',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
