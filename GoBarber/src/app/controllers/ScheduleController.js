38ecde04805e017a51d78c580c465c3a402e9d84
camposLuis <luis.campos.henrique@hotmail.com>
2019-08-14 23:26:18 -0300

    Listando agenda do prestador

---
+++ GoBarber/src/app/controllers/ScheduleController.js
@@ -0,0 +1,39 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class SheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkUserProvider) {
+      return res.json({ error: 'User is not provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+      attributes: ['id', 'date', 'user_id', 'provider_id'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new SheduleController();
